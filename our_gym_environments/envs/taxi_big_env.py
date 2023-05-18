ba16ae4fa9ef6558c9c827c98f71ebf139a37daa
KimoInaoe <arquimides.mendez@inaoep.mx>
2023-05-18 17:29:03 -0600

    - Refactor

--- our_gym_environments/envs/taxi_big_env.py
+++ our_gym_environments/envs/taxi_big_env.py
@@ -2,9 +2,7 @@ from contextlib import closing
 from io import StringIO
 from os import path
 from typing import Optional
-
 import numpy as np
-
 from gymnasium import Env, spaces, utils
 from gymnasium.envs.toy_text.utils import categorical_sample
 from gymnasium.error import DependencyNotInstalled
--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='our_gym_environments',
     version='0.1',
-    install_requires=['gymnasium', 'pygame', 'matplotlib'],
+    install_requires=['gymnasium', 'pygame', 'matplotlib', 'numpy'],
 
 )
