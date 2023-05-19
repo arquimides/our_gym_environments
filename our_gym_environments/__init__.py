953484785d4a42e9453f3a5586433b20527babcb
KimoInaoe <arquimides.mendez@inaoep.mx>
2023-05-19 17:39:15 -0600

    - namespace 'our_gym_environments' added to env_id

--- our_gym_environments/__init__.py
+++ our_gym_environments/__init__.py
@@ -1,19 +1,19 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="CoffeeTaskEnv-v0",
+    id="our_gym_environments/CoffeeTaskEnv-v0",
     entry_point="our_gym_environments.envs:CoffeeTaskEnv",
     nondeterministic=True,
 )
 
 register(
-    id="TaxiSmallEnv-v0",
+    id="our_gym_environments/TaxiSmallEnv-v0",
     entry_point="our_gym_environments.envs:TaxiSmallEnv",
     nondeterministic=True,
 )
 
 register(
-    id="TaxiBigEnv-v0",
+    id="our_gym_environments/TaxiBigEnv-v0",
     entry_point="our_gym_environments.envs:TaxiBigEnv",
     nondeterministic=True,
 )
