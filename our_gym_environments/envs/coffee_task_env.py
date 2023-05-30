f25174a331b7c531c48e4707162ac7d5165a984b
KimoInaoe <arquimides.mendez@inaoep.mx>
2023-05-30 17:47:26 -0600

    - Added render mode "none"

--- our_gym_environments/envs/coffee_task_env.py
+++ our_gym_environments/envs/coffee_task_env.py
@@ -99,7 +99,7 @@ class CoffeeTaskEnv(Env):
     * v0: Initial versions release
     """
 
-    metadata = {"render_modes": ["human", "ansi", "rgb_array"], "render_fps": 4, "environment_type": ["stochastic", "deterministic"], "reward_type": ["original", "new"]}
+    metadata = {"render_modes": ["human", "ansi", "rgb_array", "none"], "render_fps": 4, "environment_type": ["stochastic", "deterministic"], "reward_type": ["original", "new"]}
 
     def __init__(self, render_mode=None, env_type="stochastic", reward_type = "original", render_fps=4):
 
@@ -672,8 +672,9 @@ class CoffeeTaskEnv(Env):
     def render(self):
         if self.render_mode == "ansi":
             return self._render_text()
-        else:
+        elif self.render_mode == "human" or self.render_mode == "rgb_array":
             return self._render_gui()
+        return
 
     def _render_gui(self):
 
